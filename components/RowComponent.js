41d186d00420e0c8bdca43efffc79fdbce3b2caf
Junying Liu <116390595+junyingliuaaa@users.noreply.github.com>
2023-12-06 21:17:20 -0500

    change some code

--- components/RowComponent.js
+++ components/RowComponent.js
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import { StyleSheet, Text, View, Button } from 'react-native';
 =======
 import { StyleSheet, Text, View, Button, Switch } from "react-native";
@@ -6,6 +7,11 @@ import { StyleSheet, Text, View, Button, Switch } from "react-native";
 
 // import a custom hook
 import useCustomHook from '../useCustomHook';
+=======
+import { StyleSheet, Text, View, Button } from "react-native";
+// import a custom hook
+import useCustomHook from "../useCustomHook"
+>>>>>>> Stashed changes
 
 <<<<<<< Updated upstream
 const RowComponent = (props) => {   
@@ -35,6 +41,7 @@ const styles = StyleSheet.create({
 export default RowComponent
 =======
 const RowComponent = (props) => {
+<<<<<<< Updated upstream
 	// const MyHook = useCustomHook("");
 
 	const {todo} = props
@@ -61,6 +68,30 @@ const RowComponent = (props) => {
 				{/* {MyHook.someValue} */}
 				{todoCompletionValue ? 'FINISHED' : 'PENDING'}
 			</Text>	
+=======
+	const {toggle, todoCompletionValue = props.todo.isCompleted} = useCustomHook(props.todo.isCompleted)
+
+	const btnPressed = () => {
+		// call the hook's function
+		typeof toggle === 'function' && toggle(props.todo.id)
+	};
+	return (
+		<View style={{paddingLeft:20, paddingRight:20, borderBottomWidth:1}}>
+			<View style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center', height:50}}>
+				<Text style={{marginRight:10, fontSize:18}}>{props.rowNumber}-{props.todo.name}</Text>
+				{/* output the hook's state variable to the ui */}
+				<Switch
+				testID='switch'
+				trackColor={{ false: "#767577", true: "green" }}
+				onValueChange={btnPressed}
+				value={todoCompletionValue}
+				/>
+			</View>
+			<Text style={{color:todoCompletionValue ? 'green' :'skyblue',fontSize:15}}>
+			{/* {MyHook.someValue} */}
+			{todoCompletionValue ? 'FINISHED' : 'PENDING'}
+			</Text> 
+>>>>>>> Stashed changes
 		</View>
 	);
 };
